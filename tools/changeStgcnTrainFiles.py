66951928d31ed7166dd3d182cae3dcbe02ec5cf9
QianXmY <863119860@qq.com>
2023-03-26 23:16:51 +0800

    添加注释并修复 脚本修改时会残留旧类在总结文件里 的bug

--- tools/changeStgcnTrainFiles.py
+++ tools/changeStgcnTrainFiles.py
@@ -1,8 +1,15 @@
-import argparse
 from pathlib import Path
-from _utils.PoseTransformer import readJson, writeJson, coco2017Kps2coco2017cut
 import json
 from tqdm import tqdm
+import sys
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[1]  # Project root directory: D:\_NewCode\PythonPro\Phone_Walking_Detector
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+
+from _utils.PoseTransformer import readJson, writeJson, coco2017Kps2coco2017cut
+
 
 def transform(
     new_class = ['phone', 'standing'],
@@ -27,20 +34,19 @@ def transform(
         inputSumDict= readJson(input_root_dir / input_json)
         classCountThres = oldClassCountThres[i]
         for file in tqdm(Path.iterdir(input_dir), desc=part):
-            filename = file.stem
+            filename = file.stem # 获得无后缀名的文件名称
             d = readJson(file)
-            label_index = d['label_index']
-            newLabelIndex = oldClass2newClassMap[label_index]
-            if newLabelIndex == -1:
-                if filename in inputSumDict:
-                    del inputSumDict[filename]
+            label_index = d['label_index']  # 获得旧类别号
+            newLabelIndex = oldClass2newClassMap[label_index] # 获得新类别号
+            if newLabelIndex == -1: # 新类别号==-1，说明被删除
+                del inputSumDict[filename]  # 在总结json文件中删除
                 continue
-            if classCountThres[label_index] != 0:
+            if classCountThres[label_index] != 0: # 新类别的限制数量为正值
                 classCountThres[label_index] -= 1
-                newLabel = classNames[newLabelIndex]
-                d['label_index'] = newLabelIndex
-                d['label'] = newLabel
-                if skeleonTransformer:
+                newLabel = classNames[newLabelIndex]    #获得新类别名称
+                d['label_index'] = newLabelIndex    # 修改类别号为新类别号
+                d['label'] = newLabel   # 修改名称为新类别名称
+                if skeleonTransformer:  # 如果有骨骼结点转换函数
                     for kpDict in d['data']:
                         sk = kpDict['skeleton']
                         for psAdsc  in sk:
@@ -49,6 +55,8 @@ def transform(
                 inputSumDict[filename]['label_index'] = newLabelIndex
                 inputSumDict[filename]['label'] = newLabel
                 writeJson(d, output_dir / input_dir_name, filename + '.json')
+            else:
+                del inputSumDict[filename]  # 在总结json文件中删除
         writeJson(inputSumDict, output_dir, input_json)
 
 
@@ -78,9 +86,9 @@ def kineticsFormatMscocoKp2MscococutKp(keypoints):
 
 if __name__ == '__main__':
     transform(
-        new_class=['call', 'oneHand', 'twoHands', 'stand'],
-        oldClass2newClassMap=[0, 1, 2, -1, 3, -1, -1],
-        oldClassCountThres=([-1, -1, 380, -1, -1, -1, -1], [-1, -1, 90, -1, -1, -1, -1]),
+        new_class=['nohand', 'oneHand', 'twohands'],
+        oldClass2newClassMap=[-1, 1, 2, -1, 0, -1, -1],
+        oldClassCountThres=([-1, 360, 360, -1, -1, -1, -1], [-1, -1, 90, -1, -1, -1, -1]),
         input_root_dir = 'D:/_NewCode/PythonPro/st_gcn/st-gcn/stgcnTrainData/Mscoco/exp10',
         # skeleonTransformer = [kineticsFormatMscocoKp2MscococutKp, coco2017Kps2coco2017cut]
     )
