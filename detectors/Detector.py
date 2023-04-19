191bcc268eb1c4c8fb430eff50fefe8fde10330b
QianXmY <863119860@qq.com>
2023-04-19 15:14:33 +0800

    little change

--- detectors/Detector.py
+++ detectors/Detector.py
@@ -1,11 +1,15 @@
+# from abc import abstractclassmethod, abstractproperty, abstractmethod
+
 class Detector:
     def __init__(self, class_names):
         self.__class_names = class_names
 
+    # @abstractmethod
     def detectSingleImage(self, im0, conf_thres, mode = 'image', isNew = True, line_thickness = 2):
         pass
 
-    def getNames(self):
+    @property
+    def class_names(self):
         return self.__class_names
     
     def getLabel(self, cls):
